13f0a30ba50fe3ed238f77a885dd36269ba9492d
Milen Voutchev <milen.voutchev@rocket-internet.de>
2017-05-10 11:00:41 +0200

    basic ajax request to fetch weekNumber

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,25 @@
+import 'whatwg-fetch';
 
-export const refreshAll = () => ({
-  type: 'REFRESH_ALL',
+export const requestWeekNumber = () => ({
+  type: 'REQUEST_WEEKNUMBER',
 });
+
+export const receiveWeekNumber = (json) => ({
+  type: 'RECEIVE_WEEKNUMBER',
+  week: json.data.week,
+  receivedAt: Date.now(),
+});
+
+export const fetchWeekNumber = () => dispatch => {
+  dispatch(requestWeekNumber());
+  return fetch('http://mockbin.org/bin/af0bab28-d7f8-4ff5-8131-53ffd952abe2')
+    .then(response => response.json())
+    .then(json => dispatch(receiveWeekNumber(json)));
+};
+
+export const refreshAll = () => dispatch => {
+  dispatch(fetchWeekNumber());
+};
 
 export const refreshSome = () => ({
   type: 'REFRESH_SOME',
