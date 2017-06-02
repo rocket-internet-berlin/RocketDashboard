3487239f0fe55a938e75a36dac5f8ad503de082f
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2017-06-02 19:15:38 +0200

    data is fetched from a google spreadsheet

--- server/src/routes/bugsHistory.js
+++ server/src/routes/bugsHistory.js
@@ -2,7 +2,34 @@ import express from 'express';
 
 const router = express.Router();
 
+const google = require('googleapis');
+const sheets = google.sheets('v4');
+
+const jwtClient = new google.auth.JWT(
+  '',   // TODO: client email
+  null,
+  '', // TODO: private key
+  ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+  null
+);
+
 router.get('/', (req, res, next) => {
+  const request = {
+    spreadsheetId: '',    // TODO: spreadsheet ID
+    ranges: ['A1:A9', 'B1:B9'],
+    includeGridData: true,
+    auth: jwtClient,
+    fields: 'sheets(data(rowData(values(userEnteredValue/numberValue))))',
+  };
+  sheets.spreadsheets.get(request, function(err, response) {
+    if (err) {
+      console.log('ERR123: ' + err);
+      // TODO: handle an error
+    } else {
+      console.log('RESP123: ' + JSON.stringify(response, null, 2));
+      // TODO: handle a response
+    }
+  });
   res.json({
     status: 'success',
     message: '',
