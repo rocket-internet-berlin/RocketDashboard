a7408670e8ee30c47a0e93b502422c1ca6aa3353
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2017-06-08 15:01:26 +0200

    a real week number is returned from the server

--- server/src/routes/weekNumber.js
+++ server/src/routes/weekNumber.js
@@ -2,12 +2,24 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const getWeekNumber = () => {
+  const today = new Date();
+  const day = (today.getDay() + 6) % 7;
+  today.setDate((today.getDate() - day) + 3);
+  const firstThursday = today.valueOf();
+  today.setMonth(0, 1);
+  if (today.getDay() !== 4) {
+    today.setMonth(0, 1 + (((4 - today.getDay()) + 7) % 7));
+  }
+  return 1 + Math.ceil((firstThursday - today) / 604800000);
+};
+
+router.get('/', (req, res) => {
   res.json({
     status: 'success',
     message: '',
     data: {
-      week: 52,
+      week: getWeekNumber(),
     },
   });
 });
