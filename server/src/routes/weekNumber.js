33abddaa078032af9a5d5b9936e9af37ce8ef1cd
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2017-06-08 15:56:14 +0200

    moment is used

--- server/src/routes/weekNumber.js
+++ server/src/routes/weekNumber.js
@@ -1,25 +1,14 @@
 import express from 'express';
+import moment from 'moment';
 
 const router = express.Router();
-
-const getWeekNumber = () => {
-  const today = new Date();
-  const day = (today.getDay() + 6) % 7;
-  today.setDate((today.getDate() - day) + 3);
-  const firstThursday = today.valueOf();
-  today.setMonth(0, 1);
-  if (today.getDay() !== 4) {
-    today.setMonth(0, 1 + (((4 - today.getDay()) + 7) % 7));
-  }
-  return 1 + Math.ceil((firstThursday - today) / 604800000);
-};
 
 router.get('/', (req, res) => {
   res.json({
     status: 'success',
     message: '',
     data: {
-      week: getWeekNumber(),
+      week: moment().format('w'),
     },
   });
 });
