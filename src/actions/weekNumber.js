8299af3723b55d119748c3d8a21707193e3b395b
Milen Voutchev <milen.voutchev@rocket-internet.de>
2017-06-09 11:49:56 +0200

    Move weekNumber logic to client; remove API endpoint

--- src/actions/weekNumber.js
+++ src/actions/weekNumber.js
@@ -1,14 +1,12 @@
-import 'whatwg-fetch';
 import { createAction } from 'redux-actions';
-import fetchApi from './fetchApi';
+import moment from 'moment';
 
-export const requestWeekNumber = createAction('REQUEST_WEEKNUMBER');
 export const receiveWeekNumber = createAction(
   'RECEIVE_WEEKNUMBER',
-  json => json.data.week,
+  week => week,
 );
 
 export const fetchWeekNumber = () => dispatch => {
-  dispatch(requestWeekNumber());
-  return fetchApi('weekNumber').then(json => dispatch(receiveWeekNumber(json)));
+  const getWeek = () => parseInt(moment().format('w'), 10);
+  return dispatch(receiveWeekNumber(getWeek()));
 };
