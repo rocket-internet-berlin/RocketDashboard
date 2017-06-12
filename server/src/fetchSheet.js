f447321af77de6b8712eaa1ff8d54c5094fdb203
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2017-06-12 16:37:55 +0200

    Sheets API is integrated (#22)
    * data is fetched from a google spreadsheet
    
    * data is fetched, parsed and propagated to front-end
    
    * a period is parsed
    
    * linter errors and warnings are fixed
    
    * maxEntrieAmount is exposed as a constant
    
    * backend doesn't crash if the sheet is invalid
    
    * config management: dev and prod
    
    * google sheets related code is exposed to a separated file
    
    * Refactor loading of config files;
    Add JSON schema validation for expected config object
    
    * Refactor to import only used lodash methods, rather than the whole library

---
+++ server/src/fetchSheet.js
@@ -0,0 +1,70 @@
+import _get from 'lodash/get';
+import google from 'googleapis';
+import Ajv from 'ajv';
+import config from './config/config';
+
+// validate required config props
+const ajv = new Ajv({
+  allErrors: true,
+});
+const schema = {
+  'required': [
+    'spreadsheetId',
+    'clientAccountEmail',
+    'clientAccountPrivateKey',
+  ],
+};
+
+const validate = ajv.compile(schema);
+if (!validate(config.bugsDiff)) {
+  throw ajv.errorsText(validate.errors);
+}
+
+const sheets = google.sheets('v4');
+
+const jwtClient = new google.auth.JWT(
+  config.bugsDiff.clientAccountEmail,
+  null,
+  config.bugsDiff.clientAccountPrivateKey,
+  ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+  null,
+);
+
+const maxEntriesAmount = 20;  // FIXME: magic number
+
+const fetchSheet = (callback) => {
+  const request = {
+    spreadsheetId: config.bugsDiff.spreadsheetId,
+    ranges: [`A1:A${maxEntriesAmount}`, `B1:B${maxEntriesAmount}`, `C1:C${maxEntriesAmount}`],
+    includeGridData: true,
+    auth: jwtClient,
+    fields: 'sheets(data(rowData(values(userEnteredValue))))',
+  };
+  sheets.spreadsheets.get(request, (err, response) => {
+    if (err) {
+      callback(false);
+      return;
+    }
+    try {
+      // @TODO Decouple "fetching" and "data crunching" so we can reuse them
+      const data = _get(response, 'sheets[0].data');
+      const columns = data.map((column) => column.rowData.map(cellData => _get(cellData, 'values[0].userEnteredValue')));
+      const history = columns[0].map((label, i) => (
+        { label: label.stringValue, bugs: columns[1][i].numberValue }
+      ));
+      const period = columns[2][0].stringValue;
+      callback(true, {
+        status: 'success',
+        message: '',
+        data: {
+          period,
+          history,
+        },
+      });
+    } catch (exception) {
+      callback(false);
+    }
+  });
+};
+
+export default fetchSheet;
--- server/src/routes/bugsHistory.js
+++ server/src/routes/bugsHistory.js
@@ -1,36 +1,15 @@
 import express from 'express';
+import fetchSheet from '../fetchSheet';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.json({
-    status: 'success',
-    message: '',
-    data: {
-      period: 'Last 5 Days',
-      history: [
-        {
-          label: 'Mon',
-          bugs: 12,
-        },
-        {
-          label: 'Tue',
-          bugs: 8,
-        },
-        {
-          label: 'Wed',
-          bugs: 9,
-        },
-        {
-          label: 'Thu',
-          bugs: 10,
-        },
-        {
-          label: 'Fri',
-          bugs: 7,
-        },
-      ],
-    },
+router.get('/', (req, res) => {
+  fetchSheet((success, json) => {
+    if (success) {
+      res.json(json);
+    } else {
+      res.status(500).send('Sorry, something went wrong');
+    }
   });
 });
 
