eb1a9a22140373a2ea8fb2c9550666eb9b9587b4
Milen Voutchev <milen.voutchev@rocket-internet.de>
2017-06-14 15:38:52 +0200

    Fix error handling in routes

--- server/src/index.js
+++ server/src/index.js
@@ -42,13 +42,14 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.send({
     status: 'error',
     message: 'Something went terribly wrong!',
+    error: res.locals.error,
   });
 });
 
