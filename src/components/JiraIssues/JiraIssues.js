04a8045a81b75bf168787a448874f700665b92db
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2017-06-26 15:51:39 +0200

    labels below jira issues bars are aligned properly

--- src/components/JiraIssues/JiraIssues.js
+++ src/components/JiraIssues/JiraIssues.js
@@ -11,31 +11,29 @@ const JiraIssues = () => (
     <div className="panel-heading">Jira Issues</div>
     <div className="panel-body hidden-xs">
       <div className="row">
-        <div className="chart-container">
-          <ResponsiveContainer width="100%" height={200}>
-            <BarChart width="100%" height={200} data={data}>
-              <Bar
-                dataKey="blockers"
-                fill="#ff2b19"
-                label={{ fill: '#ff2b19', fontSize: 28 }}
-              />
-              <Bar
-                dataKey="criticals"
-                fill="#ff796e"
-                label={{ fill: '#ff796e', fontSize: 28 }}
-              />
-              <Bar
-                dataKey="others"
-                fill="#ffb8b2"
-                label={{ fill: '#ffb8b2', fontSize: 28 }}
-              />
-            </BarChart>
-          </ResponsiveContainer>
-          <div className="labels-container">
-            <div className="label label-blockers">blockers</div>
-            <div className="label label-criticals">criticals</div>
-            <div className="label label-others">others</div>
-          </div>
+        <ResponsiveContainer width="100%" height={190}>
+          <BarChart width="100%" height="100%" data={data}>
+            <Bar
+              dataKey="blockers"
+              fill="#ff2b19"
+              label={{ fill: '#ff2b19', fontSize: 28 }}
+            />
+            <Bar
+              dataKey="criticals"
+              fill="#ff796e"
+              label={{ fill: '#ff796e', fontSize: 28 }}
+            />
+            <Bar
+              dataKey="others"
+              fill="#ffb8b2"
+              label={{ fill: '#ffb8b2', fontSize: 28 }}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+        <div className="labels-container">
+          <div className="label label-blockers">blockers</div>
+          <div className="label label-criticals">criticals</div>
+          <div className="label label-others">others</div>
         </div>
       </div>
     </div>
