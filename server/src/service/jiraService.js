407c9ff3f5dba91fb9311c15248fd9ac88e77d79
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2017-06-30 16:57:50 +0200

    code improvement

--- server/src/service/jiraService.js
+++ server/src/service/jiraService.js
@@ -17,17 +17,8 @@ class JiraService {
     this.jira.searchJira("project = CC AND status in ('Selected for Development', 'In Progress', 'Waiting Approval')")
       .then(response => {
         const issues = response.issues;
-        let blockers = 0;
-        let criticals = 0;
-
-        issues.forEach(issue => {
-          const priority = issue.fields.priority.name;
-          if (priority === 'Blocker') {
-            blockers += 1;
-          } else if (priority === 'Critical') {
-            criticals += 1;
-          }
-        });
+        const blockers = issues.filter(issue => issue.fields.priority.name === 'Blocker').length;
+        const criticals = issues.filter(issue => issue.fields.priority.name === 'Critical').length;
         const others = issues.length - blockers - criticals;
         callback(null, { blockers, criticals, others });
       })
