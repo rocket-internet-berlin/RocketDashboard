9c32a37b27c84df5091463b7e3efc4f5f0b9ac30
milenvoutchev <milen.voutchev@rocket-internet.de>
2017-07-04 17:41:55 +0200

    use generic Number instead of NewRelicLoadTime, NRErrors, WeekNumber (#39)

--- src/components/Number/Number.js
+++ src/components/Number/Number.js
@@ -1,33 +1,36 @@
-/* eslint-disable react/forbid-prop-types */
 import React from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './Number.scss';
 
-const Number = ({ title, key, numberState }) =>
+const Number = ({ title, data }) =>
   <div className="Number panel">
     <div className="panel-heading">
       {title}
     </div>
     <div className="panel-body">
       <span className="this-week number-big">
-        {numberState[key].current}
+        {data.current}
       </span>
-      {this.props.numberState[key].previous &&
+      {data.previous &&
         <span className="last-week number-normal">
-          / {numberState[key].previous}
+          / {data.previous}
         </span>}
     </div>
   </div>;
 
-const mapStateToProps = state => ({
-  numberState: state.number,
-});
-
 Number.propTypes = {
   title: PropTypes.string.isRequired,
-  key: PropTypes.string.isRequired,
-  numberState: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    current: PropTypes.number.isRequired,
+    previous: PropTypes.number,
+  }),
 };
 
-export default connect(mapStateToProps)(Number);
+Number.defaultProps = {
+  data: {
+    current: null,
+    previous: null,
+  },
+};
+
+export default Number;
