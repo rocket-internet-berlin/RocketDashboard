c588e899291aab10834b6cd7590259e6b6b14f59
Milen Voutchev <milen.voutchev@rocket-internet.de>
2017-07-04 17:36:02 +0200

    use generic Number instead of NewRelicLoadTime, NRErrors, WeekNumber

--- src/widgets/NewRelicErrors/components/NewRelicErrors.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Number from '../../../../src/components/Number/Number';
-
-const NewRelicErrors = () => <Number title="Transaction Errors" key="newRelicErrors" />;
-
-export default NewRelicErrors;
-
-// Note: PureNewRelicErrors is NewRelicErrors without redux (for unit tests)
-export { NewRelicErrors as PureNewRelicErrors };
