415c8dfd1548d163f8c5422a270bb5292c6e80ed
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2017-07-05 16:56:21 +0200

    minor changes

--- src/widgets/Number/actions/number.test.js
+++ src/widgets/Number/actions/number.test.js
@@ -1,6 +1,6 @@
 import onReceive from '../../Number/actions/number';
 
-describe('weekNumber refresh action', () => {
+describe('onReceive action', () => {
   const actual = onReceive('someKey', 9, 99);
   const expected = { type: 'RECEIVE_NUMBER', payload: { key: 'someKey', current: 9, previous: 99 } };
 
--- src/widgets/Number/components/Number.test.js
+++ src/widgets/Number/components/Number.test.js
@@ -3,15 +3,15 @@ import { shallow } from 'enzyme';
 import Number from './Number';
 
 describe('Number component', () => {
-  const widget = shallow(<Number title="Test" data={{ current: 111, previous: 222 }} />);
+  const widget = shallow(<Number title="Some number" data={{ current: 9, previous: 99 }} />);
 
   it('contains a title', () => {
-    expect(widget.contains('Test')).toEqual(true);
+    expect(widget.contains('Some number')).toEqual(true);
   });
   it('contains a current value', () => {
-    expect(widget.contains(111)).toEqual(true);
+    expect(widget.contains(9)).toEqual(true);
   });
   it('contains a previous value', () => {
-    expect(widget.contains(222)).toEqual(true);
+    expect(widget.contains(99)).toEqual(true);
   });
 });
