ec2e9ea50cf26eaa67dd1cb5a44e2924879365b6
Milen Voutchev <milen.voutchev@rocket-internet.de>
2017-07-05 11:11:11 +0200

    tweaks

--- src/widgets/NewRelicLoadTime/actions/newRelicLoadTime.js
+++ src/widgets/NewRelicLoadTime/actions/newRelicLoadTime.js
@@ -1,9 +1,10 @@
 import 'whatwg-fetch';
+import _round from 'lodash/round';
 import fetchApi from '../../../lib/fetchApi';
 import onReceive from '../../Number/actions/number';
 
 const refreshNewRelicLoadTime = () => dispatch => {
-  fetchApi('newRelic/loadTime').then(json => dispatch(onReceive('newRelicLoadTime', json.data.current)));
+  fetchApi('newRelic/loadTime').then(json => dispatch(onReceive('newRelicLoadTime', _round(json.data.current, 2))));
 };
 
 export default refreshNewRelicLoadTime;
