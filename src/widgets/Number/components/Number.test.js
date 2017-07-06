4224065871e30a5e18ef4eff95c5158eb1ed5fba
Artem Stepanenko <artem.stepanenko@rocket-internet.de>
2017-07-06 12:13:45 +0200

    percentage is handled

--- src/widgets/Number/components/Number.test.js
+++ src/widgets/Number/components/Number.test.js
@@ -3,15 +3,15 @@ import { shallow } from 'enzyme';
 import Number from './Number';
 
 describe('Number component', () => {
-  const widget = shallow(<Number title="Some number" data={{ current: 9, previous: 99 }} />);
+  const widget = shallow(<Number title="Some number" data={{ current: 99, previous: 9 }} />);
 
   it('contains a title', () => {
     expect(widget.contains('Some number')).toEqual(true);
   });
   it('contains a current value', () => {
-    expect(widget.contains(9)).toEqual(true);
-  });
-  it('contains a previous value', () => {
     expect(widget.contains(99)).toEqual(true);
+  });
+  it('contains percentage', () => {
+    expect(widget.contains(1000)).toEqual(true);
   });
 });
