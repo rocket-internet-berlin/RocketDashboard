557dd09ed14ec60a592e10cb12a0206a612a5ccc
milenvoutchev <milen.voutchev@rocket-internet.de>
2017-07-06 11:59:32 +0200

    change compare/previous to percentage (#41)
    * change compare/previous to percentage
    
    * add font-awesome
    
    * better change percentage display; rename scss variables to be more specific; rename className;
    
    * better change percentage display; rename scss variables to be more specific; rename className;

--- src/widgets/NewRelicLoadTime/actions/newRelicLoadTime.js
+++ src/widgets/NewRelicLoadTime/actions/newRelicLoadTime.js
@@ -1,10 +1,9 @@
 import 'whatwg-fetch';
-import _round from 'lodash/round';
 import fetchApi from '../../../lib/fetchApi';
 import onReceive from '../../Number/actions/number';
 
 const refreshNewRelicLoadTime = () => dispatch => {
-  fetchApi('newRelic/loadTime').then(json => dispatch(onReceive('newRelicLoadTime', _round(json.data.current, 2))));
+  fetchApi('newRelic/loadTime').then(json => dispatch(onReceive('newRelicLoadTime', json.data.current)));
 };
 
 export default refreshNewRelicLoadTime;
--- src/widgets/Number/components/Number.js
+++ src/widgets/Number/components/Number.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _round from 'lodash/round';
 import './Number.scss';
+
+const getRounded = decimal => _round(decimal, 2);
+const getChange = (current, previous) => {
+  // https://ux.stackexchange.com/questions/60902/displaying-percentage-difference-from-zero
+  if (current > 0 && previous === 0) {
+    return 100;
+  } else if (current === 0 && previous === 0) {
+    return 0;
+  }
+  return (current - previous) / previous * 100;
+};
+
+const getChangeClassName = number => {
+  if (number < 0) {
+    return 'change decrease';
+  } else if (number > 0) {
+    return 'change increase';
+  }
+  return 'change';
+};
 
 const Number = ({ title, data }) =>
   <div className="Number panel">
@@ -8,12 +29,12 @@ const Number = ({ title, data }) =>
       {title}
     </div>
     <div className="panel-body">
-      <span className="this-week number-big">
-        {data.current}
+      <span className="current">
+        {getRounded(data.current)}
       </span>
-      {data.previous &&
-        <span className="last-week number-normal">
-          / {data.previous}
+      {typeof data.previous !== 'undefined' &&
+        <span className={getChangeClassName(getChange(data.current, data.previous))}>
+          {getRounded(getChange(data.current, data.previous))}%
         </span>}
     </div>
   </div>;
